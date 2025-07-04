0e41bbb1591cb1317eae9fd9aa6e55e74ae62b8c
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-07-04 21:31:51 +0200

    use .env in backend

--- backend/src/main/java/dev/kalbarczyk/AIRecruiter/AiRecruiterApplication.java
+++ backend/src/main/java/dev/kalbarczyk/AIRecruiter/AiRecruiterApplication.java
@@ -1,5 +1,6 @@
 package dev.kalbarczyk.AIRecruiter;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,19 @@ public class AiRecruiterApplication {
 
   public static void main(String[] args) {
 
+    try {
+      Dotenv dotenv = Dotenv.load();
+      dotenv
+          .entries()
+          .forEach(
+              entry -> {
+                System.setProperty(entry.getKey(), entry.getValue());
+                System.out.println(
+                    "DEBUG: Loaded env var: " + entry.getKey() + "=" + entry.getValue());
+              });
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     SpringApplication.run(AiRecruiterApplication.class, args);
   }
 }
