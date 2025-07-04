718d34797ecfbd1be456f57c817bc7130a01baac
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-07-04 13:03:05 +0200

    init

---
+++ backend/src/main/java/dev/kalbarczyk/AIRecruiter/AiRecruiterApplication.java
@@ -0,0 +1,12 @@
+package dev.kalbarczyk.AIRecruiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecruiterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AiRecruiterApplication.class, args);
+  }
+}
---
+++ backend/src/test/java/dev/kalbarczyk/AIRecruiter/AiRecruiterApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.kalbarczyk.AIRecruiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRecruiterApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
