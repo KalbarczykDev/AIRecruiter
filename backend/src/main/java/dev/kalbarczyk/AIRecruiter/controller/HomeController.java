b67bc4f31251a214da27b20406bd25c4b5d232b1
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-07-04 14:22:06 +0200

    fix cross origin error :)

--- backend/src/main/java/dev/kalbarczyk/AIRecruiter/controller/HomeController.java
+++ backend/src/main/java/dev/kalbarczyk/AIRecruiter/controller/HomeController.java
@@ -2,9 +2,11 @@ package dev.kalbarczyk.AIRecruiter.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HomeController {
 
